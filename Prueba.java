4f28e9e326ceda450974d10abc53f22695f38622
Lucio-Lo-Verde <lucioloverde@gmail.com>
2020-09-25 17:52:24 -0300

    commit 25

---
+++ Prueba.java
@@ -0,0 +1,42 @@
+public class prueba{
+
+	private int entero;
+	private char caracter;
+	private String nombre;
+
+	public prueba(){
+		nombre = "Lucio";
+		entero = 0;
+		caracter = 'c';
+	}
+
+	public prueba(String n, int i, char c){
+		nombre = n;
+		entero = i;
+		caracter = c;
+	}
+
+	public int getEntero(){
+		return entero;
+	}
+
+	public char getCaracter(){
+		return caracter;
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void setEntero(int i){
+		entero = i;
+	}
+
+	public void setCaracter(char c){
+		caracter = c;
+	}
+
+	public void setNombre(String n){
+		nombre = n;
+	}
+}
